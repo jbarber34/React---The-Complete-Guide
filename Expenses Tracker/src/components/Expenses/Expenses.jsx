ae1d084e9a84624ce088bad760118aaa093de74a
James Barber <jamesbarber@twinsbaseball.com>
2022-07-30 14:45:12 -0400

    Updates to add year filter and to add expenses

--- Expenses Tracker/src/components/Expenses/Expenses.jsx
+++ Expenses Tracker/src/components/Expenses/Expenses.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter/ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {props.items.map((item) => (
-        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
-      ))}
-      ;
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((item) => (
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))}
+        ;
+      </Card>
+    </div>
   );
 }
 
