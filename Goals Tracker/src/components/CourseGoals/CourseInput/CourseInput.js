f7dbc67d405fbe6303c91ebadc1e8b08f584cbd8
James Barber <jamesbarber@twinsbaseball.com>
2022-08-03 09:45:52 -0400

    Adding CSS Modules for input and button

--- Goals Tracker/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Goals Tracker/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
-
+// import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
+
+// Styled Component Method
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? 'red' : 'black')};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
+//     background: ${(props) => (props.invalid ? '#FFD7D7' : 'transparent')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -26,7 +54,10 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* Dynamically add a class using ${} */}
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      {/* Names with hyphens need to be added like below example, rather than example in Button.js */}
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
--- Goals Tracker/src/components/UI/Button/Button.js
+++ Goals Tracker/src/components/UI/Button/Button.js
@@ -1,33 +1,41 @@
-import styled from 'styled-components';
-// import './Button.css';
+// import styled from 'styled-components';
+import styles from './Button.module.css';
 
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// Styled Components Version
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  &:focus {
-    outline: none;
-  }
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:focus {
+//     outline: none;
+//   }
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+// Regular CSS/CSS Modules Version
+// Using CSS Modules, it imports the CSS classes as objects, so you use {styles.button} rather than 'button'
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
