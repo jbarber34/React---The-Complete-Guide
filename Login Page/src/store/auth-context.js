245a470230e39491f3b41746be78e098243e7512
James Barber <jamesbarber@twinsbaseball.com>
2022-08-29 15:29:11 -0400

    Control full app states with useContext wrapper

--- Login Page/src/store/auth-context.js
+++ Login Page/src/store/auth-context.js
@@ -1,7 +1,45 @@
-import { createContext } from 'react';
+import { useState, createContext, useEffect } from 'react';
 
 const AuthContext = createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: () => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = () => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
