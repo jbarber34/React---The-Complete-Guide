457caa82a811159a1d4580e382070546672d91b8
James Barber <jamesbarber@twinsbaseball.com>
2022-09-08 10:24:08 -0400

    remove disabling the button on login

--- Login Page/src/components/Login/Login.jsx
+++ Login Page/src/components/Login/Login.jsx
@@ -117,7 +117,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type='submit' className={classes.btn} disabled={!formIsValid}>
+          <Button type='submit' className={classes.btn}>
             Login
           </Button>
         </div>
