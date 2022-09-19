1b8b1b7bd416d15a93cf76fbd62ab44333e9b07d
James Barber <jamesbarber@twinsbaseball.com>
2022-09-19 12:27:29 -0400

    Adding cart modal

--- Food Order App/src/components/Cart/Cart.jsx
+++ Food Order App/src/components/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
@@ -10,16 +11,17 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
-      <div className={classes.total}></div>
-      <span> Total Amount </span>
-      <span>45.98</span>
+      <div className={classes.total}>
+        <span> Total Amount </span>
+        <span>45.98</span>
+      </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
