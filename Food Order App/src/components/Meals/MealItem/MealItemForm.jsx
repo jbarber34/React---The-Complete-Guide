566f92aa6f55e2737623e01f180d7be0e3ba3812
James Barber <jamesbarber@twinsbaseball.com>
2022-09-27 09:32:43 -0400

    Implement useRef()

--- Food Order App/src/components/Meals/MealItem/MealItemForm.jsx
+++ Food Order App/src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,7 +1,10 @@
+import { useRef } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
   const submitHandler = (event) => {
     event.preventDefault();
   };
@@ -9,6 +12,7 @@ const MealItemForm = (props) => {
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label='Amount'
         input={{
           id: 'amount_' + props.id,
