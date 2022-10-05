90d039875459cceb8b7e3a1c1f4a38f25ba9a519
James Barber <jamesbarber@twinsbaseball.com>
2022-10-04 20:51:56 -0400

    Only show 'Order' button on cart if cart has items

--- Food Order App/src/components/Cart/Cart.jsx
+++ Food Order App/src/components/Cart/Cart.jsx
@@ -7,6 +7,7 @@ const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
 
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -27,7 +28,7 @@ const Cart = (props) => {
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
