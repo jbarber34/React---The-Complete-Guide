af88212ff11e6cbbc37a2fc200057e6c5d436892
James Barber <jamesbarber@twinsbaseball.com>
2022-10-19 18:02:31 -0400

    Add conditional paragraph - just practice

--- React Optimization/src/App.js
+++ React Optimization/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState();
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
     </div>
   );
 }
