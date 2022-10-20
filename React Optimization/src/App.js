764f408d28d71a38d36fe994dcb965a2cf1dcfad
James Barber <jamesbarber@twinsbaseball.com>
2022-10-20 18:07:18 -0400

    Add button functionality to show paragraph and remove on click

--- React Optimization/src/App.js
+++ React Optimization/src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState();
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
   return (
     <div className='app'>
       <h1>Hi there!</h1>
       {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
 }
