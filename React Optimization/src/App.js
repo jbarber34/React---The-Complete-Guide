de32b21f24c5be914d7c30dadc34a217f245428c
James Barber <jamesbarber@twinsbaseball.com>
2022-10-21 15:10:38 -0400

    Adding DemoOutput and removing conditional test <p> tag

--- React Optimization/src/App.js
+++ React Optimization/src/App.js
@@ -12,7 +12,6 @@ function App() {
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
