e07ce7777a358371f02bead09e137c2db5e4ba97
James Barber <jamesbarber@twinsbaseball.com>
2022-10-24 08:58:00 -0400

    Add useCallback() to paragraphHandler; Add additional button to allow toggle to show the paragraph

--- React Optimization/src/App.js
+++ React Optimization/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 
 import './App.css';
@@ -6,14 +6,25 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  // Wrap in useCallback to save this function to prevent 'new' function with each rerender
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  // Wrap in useCallback to save this function to prevent 'new' function with each rerender
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
+
   return (
     <div className='app'>
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
