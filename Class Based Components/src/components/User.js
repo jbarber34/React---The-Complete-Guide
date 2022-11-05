6912a5ad5505fec508397d120361353586ab943f
James Barber <jamesbarber@twinsbaseball.com>
2022-11-04 22:24:57 -0400

    Make the Class based version of the User functional component

--- Class Based Components/src/components/User.js
+++ Class Based Components/src/components/User.js
@@ -1,7 +1,16 @@
+import { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  // Render similar to 'return' in a functional component
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
