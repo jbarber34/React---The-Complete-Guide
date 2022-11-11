9a6af9bd8c1e941ce2a9d8c7d59240b596f0663e
James Barber <jamesbarber@twinsbaseball.com>
2022-11-11 17:21:04 -0500

    Change location of dummy data in UserFinder

--- Class Based Components/src/components/UserFinder.js
+++ Class Based Components/src/components/UserFinder.js
@@ -2,12 +2,13 @@ import { Fragment, useState, useEffect } from 'react';
 import classes from './UserFinder.module.css';
 import Users from './Users';
 
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+
 const UserFinder = () => {
-  const DUMMY_USERS = [
-    { id: 'u1', name: 'Max' },
-    { id: 'u2', name: 'Manuel' },
-    { id: 'u3', name: 'Julie' },
-  ];
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState('');
 
